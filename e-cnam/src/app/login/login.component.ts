5498ecbb8ebf7ca9a24abdb2b7a8643f939adca4
MedVall <medvallmostafa@gmail.com>
2022-07-11 16:18:10 +0000

    html

--- e-cnam/src/app/login/login.component.ts
+++ e-cnam/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  
+  user: User =new User();
+  msg='null';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  loginUser(){
+    alert("ggggggggggggg")
+  }
 }
